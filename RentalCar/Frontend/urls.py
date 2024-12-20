6f43bee8ec766cf22636fb03b45e673cfac277d0
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2024-12-20 05:47:19 +0100

    Update urls.py

--- RentalCar/Frontend/urls.py
+++ RentalCar/Frontend/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     # Rental-related URLs
-    path('rentals/', views.RentalListView.as_view(), name='rental_list'),  # List all rentals
-    path('rentals/new/', views.CreateRentalView.as_view(), name='create_rental'),  # Create a new rental
-    path('rentals/<int:pk>/update/', views.UpdateRentalView.as_view(), name='update_rental'),  # Update a rental (return car)
-    path('rentals/<int:pk>/delete/', views.DeleteRentalView.as_view(), name='delete_rental'),  # Delete a rental
+    path('', views.RentalListView.as_view(), name='rental_list'),  # List all rentals
+    path('new/', views.CreateRentalView.as_view(), name='create_rental'),  # Create a new rental
+    path('<int:pk>/update/', views.UpdateRentalView.as_view(), name='update_rental'),  # Update a rental (return car)
+    path('<int:pk>/delete/', views.DeleteRentalView.as_view(), name='delete_rental'),  # Delete a rental
 
     # Driver-related URLs
     path('drivers/', views.DriverListView.as_view(), name='driver_list'),  # List all drivers
