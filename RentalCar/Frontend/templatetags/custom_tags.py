de7a9b5e194d985d9129f014e770351e8a0c5267
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2024-12-20 05:46:56 +0100

    update rental
    update rental form
    add custom tag
    test ok

---
+++ RentalCar/Frontend/templatetags/custom_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def add_class(field, css_class):
+    """
+    Adds a CSS class to a form field's widget.
+    """
+    return field.as_widget(attrs={"class": css_class})
